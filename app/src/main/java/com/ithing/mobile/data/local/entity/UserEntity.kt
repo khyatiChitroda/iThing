378f4af22fa92f3e762028fdf0af91dfe15ca1ee
khyatichitroda <khyatichitroda@gmail.com>
2026-02-09 11:37:15 +0100

    ITHING-007: add UserEntity for local storage

---
+++ app/src/main/java/com/ithing/mobile/data/local/entity/UserEntity.kt
@@ -0,0 +1,13 @@
+package com.ithing.mobile.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val email: String,
+    val profileImageUrl: String?
+)
