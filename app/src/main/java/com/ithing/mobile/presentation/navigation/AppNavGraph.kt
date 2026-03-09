1ee162442619028dafec4a27dd0848e6758709d3
khyatichitroda <khyatichitroda@gmail.com>
2026-03-09 12:10:23 +0100

    MOB-0201: Implement App Shell with TopBar, BottomNav and Drawer

--- app/src/main/java/com/ithing/mobile/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/ithing/mobile/presentation/navigation/AppNavGraph.kt
@@ -1,10 +1,13 @@
 package com.ithing.mobile.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.ithing.mobile.core.session.AuthEvent
+import com.ithing.mobile.core.session.AuthEventBus
 import com.ithing.mobile.presentation.feature.appshell.AppShell
 import com.ithing.mobile.presentation.feature.changepassword.ChangePasswordRoute
 import com.ithing.mobile.presentation.feature.forgetpassword.ForgotPasswordRoute
@@ -17,10 +20,27 @@ import com.ithing.mobile.presentation.feature.splash.SplashRoute
 fun AppNavGraph(
     navController: NavHostController
 ) {
+
+    LaunchedEffect(Unit) {
+        AuthEventBus.events.collect { event ->
+            when (event) {
+                AuthEvent.Logout -> {
+                    navController.navigate(AppDestination.Login.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            }
+        }
+    }
+
     NavHost(
         navController = navController,
         startDestination = AppDestination.Splash.route
     ) {
+
         composable(AppDestination.Splash.route) {
             SplashRoute(navController = navController)
         }
@@ -32,12 +52,14 @@ fun AppNavGraph(
                 navController = navController
             )
         }
+
         composable(AppDestination.ForgotPassword.route) {
             ForgotPasswordRoute(
                 viewModel = hiltViewModel(),
                 navController = navController
             )
         }
+
         composable(AppDestination.Home.route) {
             HomeScreen()
         }
@@ -45,14 +67,9 @@ fun AppNavGraph(
         composable(AppDestination.AppShell.route) {
             AppShell(navController = navController)
         }
-//        composable(AppDestination.Dashboard.route) {
-//            val dashboardViewModel: DashboardViewModel = hiltViewModel()
-//            DashboardRoute(viewModel = dashboardViewModel,
-//                navController = navController)
-//        }
+
         composable(AppDestination.ChangePassword.route) {
             ChangePasswordRoute(navController = navController)
         }
-
     }
 }
